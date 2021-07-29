90069c56211f672140b539acd8b66f621f2526f6
Dominic Surrao <dsurrao@hotmail.com>
2021-07-29 11:36:33 -0400

    save org unit hierarchy fields

--- msr-fetch-from-dhis2/expression.js
+++ msr-fetch-from-dhis2/expression.js
@@ -1,44 +1,37 @@
+getMetadata(['organisationUnits'], {});
+
 alterState(state => {
-    /*
-    * e.g. getMostRecentPeriods(2) returns [ '202107', '202106' ]
-    * if the current date is in July 2021
-    */
-    function getMostRecentPeriods(numPeriods) {
-        let periods = [];
-        let dateISO;
-        let date = new Date();
-        for (var i = 0; i < numPeriods; i++) {
-            dateISO = date.toISOString();
-            periods.push(`${dateISO.substr(0,4)}${dateISO.substr(5,2)}`)
-            if (i < numPeriods - 1) {
-                date.setMonth(date.getMonth() - 1);
-            }
-        }
-        return periods;
-    }
+    return {...state, organisationUnits: state.data.organisationUnits};
+})
 
-    return {...state, data: { mostRecentPeriods: getMostRecentPeriods(2).toString() }};
-});
-
+/* get monthly service report data */
 getDataValues({
-    orgUnit: 'kwIzfzpJ82z,plPz2j9VSIz', // CHAs BB01-02, BB01-01
-    period: dataValue('mostRecentPeriods'),
+    orgUnitGroup: 'iQHIwRQ8Dqf', // CHA
+    period: (state) => {
+        /*
+        * e.g. getMostRecentPeriods(2) returns [ '202107', '202106' ]
+        * if the current date is in July 2021
+        */
+        function getMostRecentPeriods(numPeriods) {
+            let periods = [];
+            let dateISO;
+            let date = new Date();
+            for (var i = 0; i < numPeriods; i++) {
+                dateISO = date.toISOString();
+                periods.push(`${dateISO.substr(0, 4)}${dateISO.substr(5, 2)}`)
+                if (i < numPeriods - 1) {
+                    date.setMonth(date.getMonth() - 1);
+                }
+            }
+            return periods;
+        }
+        return getMostRecentPeriods(3).toString();
+    },
     dataSet: 'IBhezUyCB5Q', // CHA Monthly Service Report
     includeDeleted: true
 });
 
 alterState(state => {
-    console.log(state.data);
+    console.log(state);
     return state;
 })
-
-
-// getMetadata(['dataElements', 'indicators'], {
-//     filters: ['name:like:ANC'],
-// });
-
-// todo: dynamically fetch last two months DONE
-// expand to all CHAs (org units)
-// test performance
-// can orgUnit be a parent in the hierarchy?
-// another job to map cha org units to position ids
